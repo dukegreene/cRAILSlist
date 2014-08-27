c100ba23247dbb78c519baa0a142c7407563318b
Duke Greene & Michael Kirlin <pair+dukegreene.mkirlin@devbootcamp.com>
2014-08-27 07:31:49 -0500

    debugging practice

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Crailslist::Application.initialize!
+
+# Enable logging
+# Rails.logger = Logger.new(STDOUT)
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,16 @@
 
 cars = Category.create(name: 'cars')
 pets = Category.create(name: 'pets')
-tin = Category.create(name: 'tin')
-computers = Category.create(name: 'computers')
 
-dog = Article.create(title: "Tin Dog", text: "Not how you thought that was gonna go, was it?", category: tin)
+audi = Article.create(title: "Audimobile", text: "It's not an innie. It's an Audi.", category: cars)
 
-audi = Article.create(title: "Audi for Assholes", text: "Not an innie.", category: cars)
+vw = Article.create(title: "VW Beetle", text: "Blue punch buggy!", category: cars)
+
+hot_wheels = Article.create(title: "Hot Wheels", text: "Really fast, really small, cooler if you're a kid. Like Turbo the snail.", category: cars)
+
+dog = Article.create(title: "Black Lab", text: "It sits, stays, and rolls over. Sometimes it eats too.", category: pets)
+
+cat = Article.create(title: "Kitty Cat", text: "If you put pictures of this online, people will like you more as a person.", category: pets)
+
+rock_biter = Article.create(title: "Rock Biter", text: "They used to be...such strong hands...", category: pets)
 
